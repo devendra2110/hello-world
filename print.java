ee27bcc922452d9855a901771ca3ec6b486dadda
devendra2110 <devendra.shringi@mindtree.com>
2016-06-17 15:14:46 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,10 @@
+package com.mindtree;
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi This is devendra kumar shringi");
+	}
+
+}
